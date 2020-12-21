09424fc458cc245e0ef4e5ddba4fe0b82e36905a
ericzw <ericwang13@protonmail.com>
2020-12-21 15:56:54 -0500

    Implemented and tested Selection Sort

--- Sorts.java
+++ Sorts.java
@@ -20,4 +20,39 @@ public class Sorts {
             }
         }
     }
+
+    /**
+     * selection sort of an int array.
+     * 
+     * @postcondition The array will be modified such that the elements will be in
+     *                increasing order.
+     * @param data - the elements to be sorted.
+     */
+    public static void selectionSort(int[] data) {
+        int len = data.length;
+        int min;
+        int swap;
+
+        for (int i = 0; i < len; i++) {
+            min = i;
+            for (int j = i + 1; j < len; j++) {
+                if (data[j] < data[min]) {
+                    min = j;
+                }
+            }
+            swap = data[i];
+            data[i] = data[min];
+            data[min] = swap;
+        }
+    }
+
+    /**
+     * insertion sort of an int array.
+     * 
+     * @postcondition The array will be modified such that the elements will be in
+     *                increasing order.
+     * @param data - the elements to be sorted.
+     */
+    public static void insertionSort(int[] data) {
+    }
 }
